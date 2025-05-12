71d15e75c373227743269e4583f697c75bdb967a
CaboFernando <fer.1972@hotmail.com>
2025-05-12 18:14:59 -0300

    - Criando interface e repository para entidade Usuario

---
+++ BolsaFamilia.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using BolsaFamilia.Domain.Entities;
+
+namespace BolsaFamilia.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> ListarTodos();
+        Task<Usuario> BuscarById(int id);
+        Task<Usuario> BuscarByCpf(string cpf);
+        Task AdicionarAsync(Usuario user);
+        Task AtualizarAsync(Usuario user);
+        Task RemoverAsync(string cpf);
+    }
+}
