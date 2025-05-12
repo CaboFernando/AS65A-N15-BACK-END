54e55be63269077f4040b759db5e679771053ff4
CaboFernando <fer.1972@hotmail.com>
2025-05-12 17:54:10 -0300

    - Configurando e executando migrations para criação das tabelas no banco de dados

---
+++ BolsaFamilia.Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolsaFamilia.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
