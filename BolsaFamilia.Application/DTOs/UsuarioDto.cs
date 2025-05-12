7a55e58dd4e3f9f902b80397c495eb865f7263f8
carlos.santos <carlos.santos@sp.agence.com.br>
2025-05-12 18:46:46 -0300

    - Correções no UsuarioRepository - Inicio da criação do UsuarioService

---
+++ BolsaFamilia.Application/DTOs/UsuarioDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolsaFamilia.Application.DTOs
+{
+    public class UsuarioDto
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
---
+++ BolsaFamilia.Application/Services/UsuarioService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BolsaFamilia.Application.Interfaces;
+using BolsaFamilia.Domain.Entities;
+using BolsaFamilia.Domain.Interfaces;
+
+namespace BolsaFamilia.Application.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task AdicionarAsync(Usuario user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarAsync(Usuario user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> BuscarByCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> BuscarById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoverAsync(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
