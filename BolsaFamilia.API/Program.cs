f4f9e7cebf1ad2c07c8f76c4915ada93ec9e8ed2
carlos.santos <carlos.santos@sp.agence.com.br>
2025-05-24 15:58:57 -0300

    - Liberando CORS da API

--- BolsaFamilia.API/Program.cs
+++ BolsaFamilia.API/Program.cs
@@ -41,6 +41,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
 
 builder.Services.AddSwaggerGen(c =>
 {
