b172f39e43627b40cc38792ce6eadaea8a762654
Rafael Francisco <francisco.rafael@aluno.ifsp.edu.br>
2025-05-29 13:57:26 -0300

    Create Utils and Validators
    Validator of cpf and email

---
+++ BolsaFamilia.Application/Utils/ValidatorUtils.cs
@@ -0,0 +1,14 @@
+using System.Text.RegularExpressions;
+
+namespace BolsaFamilia.Application.Utils
+{
+    public static class ValidadorUtils
+    {
+        private static readonly Regex cpfRegex = new Regex(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$");
+        private static readonly Regex emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+        public static bool CpfValido(string cpf) => !string.IsNullOrWhiteSpace(cpf) && cpfRegex.IsMatch(cpf);
+
+        public static bool EmailValido(string email) => !string.IsNullOrWhiteSpace(email) && emailRegex.IsMatch(email);
+    }
+}
