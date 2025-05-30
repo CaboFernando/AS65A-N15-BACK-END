e319eac064d9625f8ee954f779586fbf10b3237e
Rafael Francisco <rafael.palma.franc@gmail.com>
2025-05-30 11:10:27 -0300

    Update ValidatorUtils.cs

--- BolsaFamilia.Application/Utils/ValidatorUtils.cs
+++ BolsaFamilia.Application/Utils/ValidatorUtils.cs
@@ -4,7 +4,7 @@ namespace BolsaFamilia.Application.Utils
 {
     public static class ValidadorUtils
     {
-        private static readonly Regex cpfRegex = new Regex(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$");
+        private static readonly Regex cpfRegex = new Regex(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$");
         private static readonly Regex emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
 
         public static bool CpfValido(string cpf) => !string.IsNullOrWhiteSpace(cpf) && cpfRegex.IsMatch(cpf);
